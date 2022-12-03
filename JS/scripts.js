73921c4daa79201016598c70dda31087f24a603c
rodrigo <rgutierrezvielma@gmail.com>
2022-12-02 22:48:25 -0300

    a

--- JS/scripts.js
+++
@@ -1,7 +0,0 @@
-const myform = document.getElementById('myform')
-
-function MuestraDatos(){
-    var mensaje="Los datos fueron enviados correctamente y seran revisados por nuestros operadores";
-        mensaje = mensaje+
-    alert(mensaje);
-}
